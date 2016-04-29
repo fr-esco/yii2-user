448ad55e0e3fbe86b7545f58d4e62eae26f200d5
Francesco Colamonici <f.colamonici@gmail.com>
2016-04-29 12:25:47 +0200

    base rest controller

---
+++ controllers/rest/BaseController.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Dektrium project.
+ *
+ * (c) Dektrium project <http://github.com/dektrium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace dektrium\user\controllers\rest;
+
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\helpers\ArrayHelper;
+use yii\rest\Controller;
+
+/**
+ * @property \dektrium\user\Module $module
+ *
+ * @author Francesco Colamonici <f.colamonici@gmail.com>
+ */
+abstract class BasicController extends Controller
+{
+    /** @inheritdoc */
+    public function init()
+    {
+        parent::init();
+        \Yii::$app->user->enableSession = $this->module->enableSessionRest;
+    }
+
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'authenticator' => [
+                'class' => CompositeAuth::className(),
+                'authMethods' => empty($this->module->authMethodsRest) ? [
+                    HttpBearerAuth::className(),
+                ] : $this->module->authMethodsRest,
+            ],
+        ]);
+    }
+
+}
